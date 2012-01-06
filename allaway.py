4874e1bb3d32ceb5016c87c0e89789cebd0a6cb2
Ward Muylaert <ward.muylaert@gmail.com>
2012-01-06 21:22:11 +0100

    Script to set yourself away on all servers you're on

---
+++ allaway.py
@@ -0,0 +1,21 @@
+__module_author__ = "Ward Muylaert"
+__module_name__ = "AllAway"
+__module_version__ = "0.1"
+__module_description__ = "Sets you away on all the servers you are connected to."
+
+import xchat
+
+def allaway(word, word_eol, userdata):
+    channels = xchat.get_list('channels')
+    for channel in channels:
+        # Channel type 1 is a server, meaning we'll do it in every status tab
+        if channel.type == 1:
+            channel.context.command(word_eol[0][3:]) 
+    return xchat.EAT_ALL
+
+
+xchat.hook_command("allaway", allaway)
+xchat.hook_command("allback", allaway)
+
+xchat.prnt("Loaded %s v%s by %s."
+        % (__module_name__, __module_version__, __module_author__))
