c18ff0d21b382bef3432f8bcdec8c35e31b2965e
Ward Muylaert <ward.muylaert@gmail.com>
2012-01-13 16:22:42 +0100

    Basic highlight log script
    * Uses xchat's builtin highlight system to find messages of note
    * Makes a "server" window and copies highlight messages in there.
    * Reuses that window if it is already open
    * Msgs and actions look the same in it
    * Only reacts to highlights in channels, dialogs are already
      private anyway.

---
+++ highlightlog.py
@@ -0,0 +1,28 @@
+__module_author__ = 'Ward Muylaert'
+__module_name__ = 'Highlightlog'
+__module_version__ = '0.1'
+__module_description__ = 'Logs highlights to a window, per-server basis.'
+
+import xchat
+
+def catch_highlight(word, word_eol, userdata):
+    # Input string ends up being
+    # NICK line of text USERMODE
+    nick = word[0]
+    text = " ".join(word[1:-1])
+    channel = xchat.get_info('channel')
+    server = xchat.get_info('server')
+    highlighttab = xchat.find_context(channel='@highlights')
+    if not highlighttab:
+        xchat.command('NEWSERVER -noconnect @highlights')
+        highlighttab = xchat.find_context(channel='@highlights')
+    highlighttab.prnt('%s/%s <%s> %s' % (server, channel, nick, text))
+    return xchat.EAT_NONE
+
+
+xchat.hook_print('Channel Msg Hilight', catch_highlight)
+xchat.hook_print('Channel Action Hilight', catch_highlight)
+
+xchat.prnt('Loaded %s v%s by %s.'
+        % (__module_name__, __module_version__, __module_author__))
+
