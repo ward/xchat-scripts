fdd71d823f774a83516bf2f69df5f709c93e376b
Ward Muylaert <ward.muylaert@gmail.com>
2012-01-24 22:42:53 +0100

    xchat inspect - add context
    Check attributes and methods of context too. Sadly it lacks a __dict__
    so the info is less complete.

--- xchat_inspect.py
+++ xchat_inspect.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __module_author__ = 'Ward Muylaert'
-__module_name__ = 'XChat Inspect'
+__module_name__ = 'XChatInspect'
 __module_version__ = '0.1'
 __module_description__ = 'Print the interface of the xchat module'
 
@@ -9,8 +9,14 @@ import xchat
 
 def inspect(word, word_eol, userdata):
     methods = xchat.__dict__
-    for key in methods:
-        print chr(2), key, chr(2), methods[key]
+    xchat.prnt('xchat module:')
+    for key in sorted(methods.iterkeys()):
+        xchat.prnt('%s%s%s: %s' % (chr(2), key, chr(2), methods[key]))
+    context = xchat.get_context()
+    methods = dir(context)
+    xchat.prnt('context:')
+    for key in sorted(methods):
+        xchat.prnt('%s%s%s' % (chr(2), key, chr(2)))
     return xchat.EAT_NONE
 
 xchat.hook_command('xchatinspect', inspect)
