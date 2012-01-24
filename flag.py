588b1122a558f8807fcf9a3b3b239fb6d27bc2a3
Ward Muylaert <ward.muylaert@gmail.com>
2012-01-24 20:51:06 +0100

    Flags of the World!... eventually
    Only got BEL, NLD and FRA for the moment. I'm thinking flags will only
    be added when the need occurs.

---
+++ flag.py
@@ -0,0 +1,35 @@
+__module_author__ = 'Ward Muylaert'
+__module_name__ = 'Flags'
+__module_version__ = '0.1'
+__module_description__ = ''
+
+import xchat
+
+_country_to_flag_dict = {
+        'BEL': '\k01,01****\k08,08****\k04,04****\n\k01,01****\k08,08****\k04,04****\n\k01,01****\k08,08****\k04,04****'
+        , 'NLD': '\k04,04************\n\k00,00************\n\k12,12************'
+        , 'FRA': '\k02,02****\k00,00****\k04,04****\n\k02,02****\k00,00****\k04,04****\n\k02,02****\k00,00****\k04,04****'
+}
+
+def country_to_flag(countrycode):
+    try:
+        flag = _country_to_flag_dict[countrycode]
+        flag = flag.replace('\\k', chr(3))
+        return flag
+    except KeyError:
+        raise LookupError
+
+def flag(word, word_eol, userdata):
+    try:
+        flag = country_to_flag(word[1]).split('\n')
+        for line in flag:
+            xchat.prnt(line)
+    except LookupError:
+        xchat.prnt("ERROR: No such country code")
+    return xchat.EAT_ALL
+
+xchat.hook_command("flag", flag)
+
+xchat.prnt('Loaded %s v%s by %s.'
+        % (__module_name__, __module_version__, __module_author__))
+
