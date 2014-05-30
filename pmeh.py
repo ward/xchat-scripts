14e775e9c994975297f33f4d6d07afc651b582f6
Ward Muylaert <ward.muylaert@gmail.com>
2014-05-30 11:03:35 +0200

    Print plugin info for pmeh on loading

--- pmeh.py
+++ pmeh.py
@@ -10,6 +10,7 @@
 
 # SOurce: https://github.com/nanotube/xchatscripts
 
+__module_author__ = "nanotube <nanotube (at) users [dot] sourceforge [dot] net >"
 __module_name__ = "PM Equals Highlight"
 __module_version__ = "0.0.1"
 __module_description__ = "Set the color of the PM tabs on message to be the same as a highlight on channel."
@@ -25,3 +26,5 @@ def on_pm(word, word_eol, userdata):
 xchat.hook_print('Private Message to Dialog', on_pm);
 xchat.hook_print('Private Action to Dialog', on_pm);
 
+xchat.prnt('Loaded %s v%s by %s.'
+        % (__module_name__, __module_version__, __module_author__))
